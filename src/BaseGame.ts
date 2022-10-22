a73e0ca3b35638ae29eae1750eb805916ee3a785
szabi1112 <banki.szabolcs@gmail.com>
2022-10-22 21:40:29 +0200

    Teljes projekt
    Javytani: Nem látom az "aranyalmát"
    Plusz feladatok közül: Piros alma 1-et ér, és az arany alma minusz 2-t ér.

--- src/BaseGame.ts
+++ src/BaseGame.ts
@@ -4,45 +4,53 @@ import Piece from "./Piece";
 import Utils from "./Utils";
 
 export default abstract class BaseGame implements IBaseGame {
-  protected moving: boolean = false;
+     head: Piece;
+     tail: Piece;
+     food: Piece | null;
+     goldenApple: Piece | null;
+     length: number = 0;
+     growth: number = 0;
+     score: number = 0;
+     currentLevel: Level | null;
+     garden: HTMLDivElement;
 
-  protected gridVisible: boolean = false;
+    protected moving: boolean = false;
 
-  protected debugSpeed: number = 0;
+    protected paused: boolean = false;
 
-  protected keyHeld: number = 0;
+    protected gridVisible: boolean = false;
 
-  protected noClip: boolean = false;
+    protected debugSpeed: number = 0;
 
-  /**
-   * @returns {number}
-   * Egy random számot szorozz meg a this.levels.length-szel,
-   * majd kerekítsd lefelé, ez lesz az index.
-   * Majd térj vissza a this.levels tömbnek ezzel az indexével.
-   */
-  abstract getRandomLevel(): Level {
-    const randomNumb = Math.random();
-    const rand = Math.floor(randomNumb*this.)
-  }
+    protected keyHeld: number = 0;
 
-  /**
-   * @returns {boolean}
-   * 1. hozz létre egy chance nevű változót 5 értékkel
-   * 2. hozz létre egy pick nevű változót, értéke random szám szorozva 100-zal
-   * 3. térj vissza true értékkel, ha a pick kisebb, mint a chance
-   */
-  abstract mayIHaveGoldenApple(): boolean;
+    protected noClip: boolean = false;
 
-    
-  /**
-   * @returns {void}
-   * A metódus feladatai:
-   * 1. keresd meg a DOM-ban az összes .vertical-grid és .horizontal-grid
-   * elemet
-   * 2. mentsd el őket egy grids nevű változóba
-   * 3. járd be a tömböt, és minden elemére hívd meg a Utils.removeNode
-   * metódust, hogy eltávolítsd őket az oldalról
-   * 4. a this.gridVisible értékét állítsd false-ra
-   */
-  abstract removeGrid(): void;
+    /**
+     * @returns {number}
+     * Egy random számot szorozz meg a this.levels.length-szel, 
+     * majd kerekítsd lefelé, ez lesz az index.
+     * Majd térj vissza a this.levels tömbnek ezzel az indexével.
+     */
+     abstract getRandomLevel(): Level;
+
+    /**
+     * @returns {boolean}
+     * 1. hozz létre egy chance nevű változót 5 értékkel
+     * 2. hozz létre egy pick nevű változót, értéke random szám szorozva 100-zal
+     * 3. térj vissza true értékkel, ha a pick kisebb, mint a chance
+     */
+     abstract mayIHaveGoldenApple(): boolean;
+
+    /**
+     * @returns {void}
+     * A metódus feladatai:
+     * 1. keresd meg a DOM-ban az összes .vertical-grid és .horizontal-grid 
+     * elemet
+     * 2. mentsd el őket egy grids nevű változóba
+     * 3. járd be a tömböt, és minden elemére hívd meg a Utils.removeNode 
+     * metódust, hogy eltávolítsd őket az oldalról
+     * 4. a this.gridVisible értékét állítsd false-ra
+     */
+     abstract removeGrid (): void;
 }
